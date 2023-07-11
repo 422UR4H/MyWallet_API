c1802d8cf038ec1ebc565ef3b53270b85cfb767e
422UR4H <tiagosindra@id.uff.br>
2023-07-11 14:03:19 -0300

    configs for deploy on render added

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
